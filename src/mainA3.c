2ebc30a6732ba91a401a4a488404afecc73a2cca
oigbinob <oiginob@uoguelph.ca>
2023-03-09 12:44:50 -0500

    Edits

--- src/mainA3.c
+++ src/mainA3.c
@@ -3,7 +3,20 @@
 int main()
 {
 
+    //Menu selection
     printf("Hello World!\n");
+
+    printf("1. Add a new employee.\n");
+    printf("2. Print data of all employees.\n");
+    printf("3. Print data of the nth employee\n");
+    printf("4. Search for employee based on empId\n");
+    printf("5. Search for employee based on full name\n");
+    printf("6. Count the total number of employees\n");
+    printf("7. Sort the employees based on their empId\n");
+    printf("8. Remove the nth employee in the current LL\n");
+    printf("9. Remove all employees in the current LL\n");
+    printf("10. Exit\n");
+
 
 
     return 0;
